35e3f76d8af38c01c529b9be0bd1ecc5ac49dc04
AliParky <aparkinson670@gmail.com>
2026-01-13 00:35:42 +0800

    Extract data field from shrine API response

--- shrine.py
+++ shrine.py
@@ -4,5 +4,5 @@ def fetch_shrine_data():
     response = requests.get("https://api.nightlight.gg/v1/shrine")
     return response.json()
 
-shrine_data = fetch_shrine_data()
+shrine_data = fetch_shrine_data()["data"]
 print(shrine_data)
