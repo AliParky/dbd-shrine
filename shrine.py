4bcce06154d4d872f3f8230861fd13edf224e03f
AliParky <aparkinson670@gmail.com>
2026-01-21 01:28:12 +0800

    Add HTTP error handling to shrine API

--- shrine.py
+++ shrine.py
@@ -3,8 +3,8 @@ import requests
 def fetch_shrine_data():
     url = "https://api.nightlight.gg/v1/shrine"
     response = requests.get(url)
+    response.raise_for_status()
     return response.json()
-
 shrine_data = fetch_shrine_data()["data"]
 
 for perk in shrine_data["perks"]:
