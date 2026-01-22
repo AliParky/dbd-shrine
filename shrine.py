dcd456e2b473e540926f1e2b547fc6f5745f8133
AliParky <aparkinson670@gmail.com>
2026-01-23 01:31:56 +0800

    Add error handling to fetch shrine data function

--- shrine.py
+++ shrine.py
@@ -2,10 +2,17 @@ import requests
 
 def fetch_shrine_data():
     url = "https://api.nightlight.gg/v1/shrine"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-shrine_data = fetch_shrine_data()["data"]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except:
+        return None
+
+shrine_data = fetch_shrine_data()
+if shrine_data:
+    shrine_data = shrine_data["data"]
 
 for perk in shrine_data["perks"]:
     print(perk)
