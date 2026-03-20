e4b4c00201383fe1687f5fd61f0e4be771345fcc
AliParky <aparkinson670@gmail.com>
2026-03-20 12:27:42 +0800

    Add week information to shrine data output

--- shrine.py
+++ shrine.py
@@ -20,6 +20,7 @@ def format_shrine_data(shrine_data):
     start_date = datetime.fromisoformat(data["start"].replace("Z", "+00:00"))
     end_date = datetime.fromisoformat(data["end"].replace("Z", "+00:00"))
     print("Dead by Daylight - Shrine of Secrets")
+    print(data['week'])
     print("Available Perks:")
     for perk in data["perks"]:
         print(f"\n{perk['name']}")
